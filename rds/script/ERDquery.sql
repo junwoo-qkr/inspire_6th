99620da876ea7a774f59bbcccacb4e276d4c30f1
junwoo-qkr <p40212@gmail.com>
2026-08-28 21:17:46 +0900

    ALTER, VIEW, DML 활용

---
+++ rds/script/ERDquery.sql
@@ -0,0 +1,128 @@
+-- 문제 1번
+DROP TABLE IF EXISTS customers;
+CREATE TABLE CUSTOMERS (
+	CNO INT(5) PRIMARY KEY,
+	CNAME VARCHAR(10) NOT NULL,
+	ADDRESS VARCHAR(50) NOT NULL,
+	EMAIL VARCHAR(20) NOT NULL,
+	PHONE VARCHAR(20) NOT NULL
+);
+
+
+DROP TABLE IF EXISTS ORDERS;
+CREATE TABLE ORDERS (
+	ORDERNO INT(10) PRIMARY KEY,
+	ORDERDATE DATE DEFAULT SYSDATE() NOT NULL,
+	ADDRESS VARCHAR(50) NOT NULL,
+	PHONE VARCHAR(20) NOT NULL,
+	STATUS VARCHAR(20) NOT NULL CHECK(STATUS IN ('결제완료', '배송중', '배송완료')),
+	CNO INT(5) NOT NULL REFERENCES CUSTOMERS(CNO)
+);
+
+
+DROP TABLE IF EXISTS PRODUCTS;
+CREATE TABLE PRODUCTS (
+	PNO INT(5) PRIMARY KEY,
+	PNAME VARCHAR(20) NOT NULL,
+	COST INT(8) DEFAULT 0 NOT NULL,
+	STOCK INT(5) DEFAULT 0 NOT NULL
+);
+
+
+DROP TABLE IF EXISTS ORDERDETAIL;
+CREATE TABLE ORDERDETAIL (
+	ORDERNO INT(10),
+	PNO INT(5),
+	QTY INT(5) DEFAULT 0,
+	COST INT(8) DEFAULT 0,
+	PRIMARY KEY(ORDERNO, PNO),
+	FOREIGN KEY(ORDERNO) REFERENCES ORDERS(ORDERNO),
+	FOREIGN KEY(PNO) REFERENCES PRODUCTS(PNO)
+);
+
+
+-- 문제 2번
+INSERT INTO products
+VALUES
+	(1001, '삼양라면', 1000, 200),
+	(1002, '새우깡', 1500, 500),
+	(1003, '월드콘', 2000, 350),
+	(1004, '빼빼로', 2000, 700),
+	(1005, '코카콜라', 1800, 550),
+	(1006, '환타', 1600, 300);
+SELECT * FROM products;
+
+
+-- 문제 3번
+INSERT INTO customers
+VALUES
+(101, '김철수', '서울 강남구', 'cskim@naver.com', '899-6666'),
+(102, '이영희', '부산 서면', 'yhlee@empal.com', '355-8882'),
+(103, '최진국', '제주 동광양', 'jkchoi@gmail.com', '852-5764'),
+(104, '강준호', '강릉 홍제동', 'jhkang@hanmail.com', '559-7777'),
+(105, '민병국', '대전 전민동', 'bgmin@hotmail.com', '559-8741'),
+(106, '오민수', '광주 북구', 'msoh@microsoft.com', '542-9988');
+SELECT * FROM customers;
+
+
+-- 문제 4번
+INSERT INTO orders(ORDERNO, ORDERDATE, ADDRESS, PHONE, STATUS, CNO)
+VALUES(
+	1, 
+	SUBDATE(SYSDATE(), 3), 
+	'서울 강남구', 
+	'899-6666', 
+	'결제완료', 
+	(SELECT CNO FROM customers WHERE CNAME = '김철수')
+);
+
+
+INSERT INTO orderdetail(ORDERNO, PNO, QTY, COST)
+VALUES(
+	1,
+	(SELECT PNO FROM products WHERE PNAME = '삼양라면'),
+	50,
+	1000
+);
+
+
+SELECT * FROM orders;
+SELECT * FROM orderdetail;
+
+
+-- 문제 5번
+UPDATE	products
+SET		STOCK = 150
+WHERE		PNAME = '삼양라면';
+
+
+-- 문제 6번
+INSERT INTO orders(ORDERNO, ORDERDATE, ADDRESS, PHONE, STATUS, CNO)
+VALUES(
+	2, 
+	SUBDATE(SYSDATE(), 2), 
+	'부산 수영구', 
+	'337-5000', 
+	'결제완료', 
+	(SELECT CNO FROM customers WHERE CNAME = '이영희')
+);
+
+
+INSERT INTO orderdetail(ORDERNO, PNO, QTY, COST)
+VALUES
+	(
+		2,
+		(SELECT PNO FROM products WHERE PNAME = '새우깡'),
+		100,
+		1500
+	),
+	(
+		2,
+		(SELECT PNO FROM products WHERE PNAME = '월드콘'),
+		150,
+		2000
+	);
+	
+
+SELECT * FROM orders;
+SELECT * FROM orderdetail;
